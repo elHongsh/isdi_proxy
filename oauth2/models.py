79f5e0c3b16d874ab01cc6f5641bcc0707bee060
qnix95@naver.com <qnix95@naver.com>
2019-08-08 00:27:35 +0900

    /admin 관리 목록에 Client 추가

--- oauth2/models.py
+++ oauth2/models.py
@@ -6,3 +6,6 @@ class Client(models.Model):
     client_name = models.TextField(null=False, blank=False, default="Unknown")
     client_id = models.TextField(null=False, blank=False, max_length=200)
     client_secret = models.TextField(null=False, blank=False, max_length=200)
+
+    def __str__(self):
+        return f"{self.client_name}({self.client_id})"
