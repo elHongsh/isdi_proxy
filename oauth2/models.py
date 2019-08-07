71e3e75c7a28c0ec6a1d62c32e7fed0d239bc1ca
qnix95@naver.com <qnix95@naver.com>
2019-08-07 22:57:42 +0900

    client의 이름 추가 저장

--- oauth2/models.py
+++ oauth2/models.py
@@ -3,5 +3,6 @@ from django.db import models
 
 # Create your models here.
 class Client(models.Model):
+    client_name = models.TextField(null=False, blank=False, default="Unknown")
     client_id = models.TextField(null=False, blank=False, max_length=200)
     client_secret = models.TextField(null=False, blank=False, max_length=200)
